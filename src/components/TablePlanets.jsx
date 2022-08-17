3f4db23f86d995b2e564644d85ddd5bc737dca58
Danubio Rafael <danubio.rafaeel@gmail.com>
2022-08-17 14:59:46 -0300

    Requisito 1 feito.

---
+++ src/components/TablePlanets.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react';
+
+export default function TablePlanets() {
+  const [planets, setPlanets] = useState([]);
+
+  useEffect(() => {
+    const fetchPlanets = async () => {
+      const fetchedPlanets = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
+      const planetsJson = await fetchedPlanets.json();
+      (planetsJson.results).forEach((planet) => {
+        delete planet.residents;
+      });
+      setPlanets(planetsJson.results);
+    };
+    fetchPlanets();
+  }, []);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Rotation Period</th>
+          <th>Orbital Period</th>
+          <th>Diameter</th>
+          <th>Climate</th>
+          <th>Gravity</th>
+          <th>Terrain</th>
+          <th>Surface Water</th>
+          <th>Population</th>
+          <th>Films</th>
+          <th>Created</th>
+          <th>Edited</th>
+          <th>URL</th>
+        </tr>
+      </thead>
+      <tbody>
+        { planets.map((planet) => (
+          <tr key={ planet.name }>
+            <th>{planet.name}</th>
+            <th>{planet.rotation_period}</th>
+            <th>{planet.orbital_period}</th>
+            <th>{planet.diameter}</th>
+            <th>{planet.climate}</th>
+            <th>{planet.gravity}</th>
+            <th>{planet.terrain}</th>
+            <th>{planet.surface_water}</th>
+            <th>{planet.population}</th>
+            <th>{planet.films}</th>
+            <th>{planet.created}</th>
+            <th>{planet.edited}</th>
+            <th>{planet.url}</th>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
